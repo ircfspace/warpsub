9ad3765a8d5df4a49463dc9833e0796019f69b28
IRCF <ircfspace@gmail.com>
2024-02-21 18:04:47 +0330

    Update crawler.php

--- crawler.php
+++ crawler.php
@@ -8,7 +8,7 @@
     $warp .= "//subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=0\n";
     $warp .= "//support-url: https://t.me/ircfspace\n";
     $warp .= "//profile-web-page-url: https://ircf.space\n\n";
-    $warp .= "warp://auto#WarpInWarp&&detour=warp://auto#Warp 🇮🇷";
+    $warp .= "warp://auto#WarpInWarp 🛡&&detour=warp://auto#Warp 🇮🇷";
 
     $i = 1;
     $pattern = '/^warp:\/\/.*$/';
